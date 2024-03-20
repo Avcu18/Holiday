2a8ad57e83601f444ed664f00c1284e675ab8324
Avcu18 <bora.avcu@rwth-aachen.de>
2024-03-21 00:35:53 +0100

    Update HolidayFunding.sol

--- HolidayFunding.sol
+++ HolidayFunding.sol
@@ -1,9 +1,12 @@
+
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.0;
 
+//HELPful: für umrechnungen https://eth-converter.com/
+
 // To-Do
-//  usdt converser needed
 // preis je nach urlaubsziel/ dynamisieren
+
 
 import "./HolidayVoting.sol";
 import "./ETHtoUSDTconverter.sol";
@@ -12,23 +15,30 @@ contract Funding is Voting {
     using Converter for uint256;
 
     address [] friends;
-    uint neededMoney; // pro person 
+    uint public neededMoney; // pro person 
     mapping(address => uint256) public addressToAmountMoney;
     string FinalWinner;
-    uint expenses;
-
+    uint  expenses;
+    // uint test = 100;
+    
+    //To-Do evtl event einfügen welches setWinner nach einer gewissen zeit/bedingung immer intalisiert
     constructor(){
         setWinner();
     }    
-
+    /*
+    function tester() public view returns(uint){
+        return test.getConversionRate();
+    }
+    */
    function setWinner() public {
     (,expenses) = winnerOfVoting();
-    neededMoney = expenses;
+    neededMoney = expenses * 1e18;
     }
 
 
     function funding() public payable   {
-        require(msg.value.getConversionRate() >= neededMoney, "You didnt send enough money"); // kann auch mehr geld schicken
+        // require(msg.value.getConversionRate() >= neededMoney, "You didnt send enough money"); => funktioniert gerade nicht
+        require(msg.value>= neededMoney, "You didnt send enough money"); // kann auch mehr geld schicken; msg.value.getConversionRate() == getConversionRate(msg.value) erster parameter wird genommen
         friends.push(msg.sender);
         addressToAmountMoney[msg.sender] = msg.value;
 
@@ -46,5 +56,8 @@ contract Funding is Voting {
     bool sendSuccess = payable(msg.sender).send(address(this).balance);
     require(sendSuccess, "Contracted couldnt be drained");
     }
+
+
+
 
 }
